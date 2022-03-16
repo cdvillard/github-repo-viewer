e44d24b2393291f401bf64472603939cc6f945f6
Charles Villard <c.d.villard@gmail.com>
2022-03-16 13:10:16 -0400

    add link to return to homepage in case of error

--- src/components/ErrorFallback.js
+++ src/components/ErrorFallback.js
@@ -1,8 +1,11 @@
+import { Link } from "react-router-dom";
+
 function ErrorFallback({ error }) {
   return (
     <div role="alert">
       <h1>Something went wrong:</h1>
       <p style={{ color: 'red' }}>{error.message}</p>
+      <a href="/">Click here to return to the homepage and try again.</a>
     </div>
   )
 }
